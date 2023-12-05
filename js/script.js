0e355bb92f055ee6fbc2e1c2df704341043431ac
abdulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2023-12-05 13:53:39 +0500

    finish

---
+++ js/script.js
@@ -0,0 +1,170 @@
+const elContainer = document.querySelector('#container');
+const elTitle = document.querySelector('#title');
+const elList = document.querySelector('#list');
+const elItem = document.querySelector('#item');
+const elItemTitle = document.querySelector('#item-title');
+const elItemText = document.querySelector('#item-text');
+const elItemLink = document.querySelector('#item-link');
+const elItem2 = document.querySelector('#item-2');
+const elItem2Title = document.querySelector('#item-2-title');
+const elItem2Text = document.querySelector('#item-2-text');
+const elItem2Link = document.querySelector('#item-2-link');
+const elItem3 = document.querySelector('#item-3');
+const elItem3Title = document.querySelector('#item-3-title');
+const elItem3Text = document.querySelector('#item-3-text');
+const elItem3Link = document.querySelector('#item-3-link');
+const elItem4 = document.querySelector('#item-4');
+const elItem4Title = document.querySelector('#item-4-title');
+const elItem4Text = document.querySelector('#item-4-text');
+const elItem4Link = document.querySelector('#item-4-link');
+
+
+// ============================= CONTAINER START ============================= //
+
+elContainer.style.width = '1150px';
+elContainer.style.margin = '0 auto';
+elContainer.style.width = '0 20px';
+
+// ============================= CONTAINER END ============================= //
+
+// ============================= TITLE START ============================= //
+
+elTitle.style.textAlign = 'center';
+elTitle.style.marginBottom = '20px';
+
+// ============================= TITLE END ============================= //
+
+// ============================= LIST START ============================= //
+
+elList.style.display = 'flex';
+elList.style.alainItems = 'center';
+elList.style.gap = '0 10px';
+elList.style.listStyle = 'none';
+
+
+// ============================= LIST END ============================= //
+
+// ============================= ITEM START ============================= //
+
+elItem.style.width = '217px';
+elItem.style.padding = '20px';
+elItem.style.background = 'linear-gradient(143deg, aqua, #aaaaff)';
+elItem.style.boxShadow = '0px 5px 3px 2px #e7e7e7bf';
+
+// ============================= ITEM END ============================= //
+
+// ============================= ITEM-TITLE START ============================= //
+
+elItemTitle.style.color = 'white';
+elItemTitle.style.fontSize = '25px';
+
+// ============================= ITEM-TITLEEND ============================= //
+
+// ============================= ITEM-TEXT START ============================= //
+
+elItemText.style.width = '229px';
+elItemText.style.color = 'white';
+elItemText.style.marginBottom = '30px';
+
+// ============================= ITEM-TEXT END ============================= //
+
+// ============================= ITEM-LINK START ============================= //
+
+elItemLink.style.color = 'white';
+elItemLink.style.textDecoration = 'none';
+
+// ============================= ITEM-LINK END ============================= //
+
+// ============================= ITEM-2 START ============================= //
+
+elItem2.style.width = '217px';
+elItem2.style.padding = '20px';
+elItem2.style.background = 'linear-gradient(143deg, #ff516f, rgb(255 152 170))';
+elItem2.style.boxShadow = '0px 5px 3px 2px #e7e7e7bf';
+
+// ============================= ITEM-2 END ============================= //
+
+// ============================= ITEM-2-TITLE START ============================= //
+
+elItem2Title.style.color = 'white';
+elItem2Title.style.fontSize = '25px';
+
+// ============================= ITEM-2-TITLE END ============================= //
+
+// ============================= ITEM-2-TEXT START ============================= //
+
+elItem2Text.style.width = '229px';
+elItem2Text.style.color = 'white';
+elItem2Text.style.marginBottom = '30px';
+
+// ============================= ITEM-2-TEXT END ============================= //
+
+// ============================= ITEM-2-LINK START ============================= //
+
+elItem2Link.style.color = 'white';
+elItem2Link.style.textDecoration = 'none';
+
+// ============================= ITEM-2-LINK END ============================= //
+
+// ============================= ITEM-3 START ============================= //
+
+elItem3.style.width = '217px';
+elItem3.style.padding = '20px';
+elItem3.style.backgroundColor = 'white';
+elItem3.style.boxShadow = '0px 5px 3px 2px #e7e7e7bf';
+
+// ============================= ITEM-3 END ============================= //
+
+// ============================= ITEM-3-TITLE START ============================= //
+
+elItem3Title.style.color = 'black';
+elItem3Title.style.fontSize = '25px';
+
+// ============================= ITEM-3-TITLE END ============================= //
+
+// ============================= ITEM-3-TEXT START ============================= //
+
+elItem3Text.style.width = '229px';
+elItem3Text.style.color = 'black';
+elItem3Text.style.marginBottom = '30px';
+
+// ============================= ITEM-3-TEXT END ============================= //
+
+// ============================= ITEM-3-LINK START ============================= //
+
+elItem3Link.style.color = 'black';
+elItem3Link.style.textDecoration = 'none';
+elItem2Link.style.marginBottom = '0px'
+
+// ============================= ITEM-3-LINK END ============================= //
+
+// ============================= ITEM-4 START ============================= //
+
+elItem4.style.width = '217px';
+elItem4.style.padding = '20px';
+elItem4.style.background = 'linear-gradient(143deg, #323131, #6d5b5b)';
+elItem4.style.boxShadow = '0px 5px 3px 2px #e7e7e7bf';
+
+// ============================= ITEM-4 END ============================= //
+
+// ============================= ITEM-4-TITLE START ============================= //
+
+elItem4Title.style.color = 'white';
+elItem4Title.style.fontSize = '25px';
+
+// ============================= ITEM-4-TITLE END ============================= //
+
+// ============================= ITEM-4-TEXT START ============================= //
+
+elItem4Text.style.width = '229px';
+elItem4Text.style.color = 'white';
+elItem4Text.style.marginBottom = '30px';
+
+// ============================= ITEM-4-TEXT END ============================= //
+
+// ============================= ITEM-4-LINK START ============================= //
+
+elItem4Link.style.color = 'white';
+elItem4Link.style.textDecoration = 'none';
+
+// ============================= ITEM-4-LINK END ============================= //
